40fb73ecc3ec50a898e4eb9fbffc78c24386e9b3
Dongfu Jiang <82598202+jdf-prog@users.noreply.github.com>
2024-08-08 16:47:30 -0700

    Update trainer.py

--- src/llamafactory/train/dpo/trainer.py
+++ src/llamafactory/train/dpo/trainer.py
@@ -135,6 +135,7 @@ class CustomDPOTrainer(DPOTrainer):
         gamma_logratios = self.simpo_gamma / self.beta
         logits = pi_logratios - gamma_logratios
         simpo_loss = -F.logsigmoid(self.beta * logits)
+        simpo_loss += chosen_logps
         return simpo_loss
 
     def compute_preference_loss(
